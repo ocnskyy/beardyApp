c22db0c67c70af381ea23108658dbf9e4d144615
serhii <ocnskyy@gmail.com>
2016-03-24 03:45:55 +0200

    geo begin

--- js/bundle.js
+++ js/bundle.js
@@ -171,7 +171,20 @@ beardyApp.controller('FileController', ['$scope', '$state', function($scope, $st
 
 beardyApp.controller('GeoController', ['$scope', '$state', function($scope, $state) {
 	console.log('its geo controller');
-
+	var query = {
+		latitude: 41.38,
+		 longitude: 2.15,
+		 radius: 100000,
+		 units: "METERS",
+	};
+	function success(data) {
+		console.log('success', data);
+	}
+	function error(data) {
+		console.log( "error message - " + err.message );
+      console.log( "error code - " + err.statusCode );
+	}
+	Backendless.Geo.find( query, new Backendless.Async( success, error ) )
 }]);
 
 beardyApp.controller('LoginController', ['$scope', '$state', function($scope, $state){
